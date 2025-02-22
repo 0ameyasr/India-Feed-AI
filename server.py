7e9dc4fd8a98c31cdc1bb72f8c5326fa7c09dbd6
Ameya Srivastava <0ameya.sr@gmail.com>
2025-02-23 03:39:43 +0530

    Added image support

--- server.py
+++ server.py
@@ -17,14 +17,10 @@ def home():
     for topic in topics:
         topic_articles = list(articles.find({"topic": topic}))
         for article in topic_articles:
-            # Convert article content to markdown
             if 'article' in article:
-                # Convert **text** to bold, *text* to italic, etc.
                 article['article'] = markdown2.markdown(article['article'])
         articles_by_topic[topic] = topic_articles
 
-    selected_topic = request.args.get("topic", "Finance")  # Default to Finance
-    
     return render_template(
         "index.html",
         topics=topics,
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -7,14 +7,14 @@ function showTopicArticles(topic) {
     topicArticles.forEach(article => {
         const li = document.createElement('li');
         li.className = 'list-group-item article-item';
-        li.onclick = () => loadArticle(article.title, article.content, article.date);
+        li.onclick = () => loadArticle(article.title, article.content, article.date, article.image_url);
         li.textContent = article.title;
         articlesList.appendChild(li);
     });
 
     if (topicArticles.length > 0) {
         const firstArticle = topicArticles[0];
-        loadArticle(firstArticle.title, firstArticle.content, firstArticle.date);
+        loadArticle(firstArticle.title, firstArticle.content, firstArticle.date, firstArticle.image_url);
     } else {
         loadArticle(
             'No Articles Available',
@@ -24,11 +24,12 @@ function showTopicArticles(topic) {
     }
 }
 
-function loadArticle(title, content, date) {
+function loadArticle(title, content, date, image_url) {
     const articleContent = document.getElementById('article-content');
     articleContent.innerHTML = `
       <h2>${title || 'No Title Available'}</h2>
       ${date ? `<p><i>${date}</i></p>` : ''}
+      <img src=${image_url} alt="Image" width="690" height="388"/>
       <div class="article-content">${content || 'No content available'}</div>
     `;
 
